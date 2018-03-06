e27731c3fe8be812bef37e4e42c2a4d104905e0e
Gopi Gokaraju <gopi.gokaraju@benefitfocus.com>
2018-03-06 19:53:55 +0530

    Adding project code

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <ul className="main-nav">
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/age">Age</NavLink></li>
+      <li><NavLink to="/gender">Gender</NavLink></li>
+      <li><NavLink to="/race">Race</NavLink></li>
+      <li><NavLink to="/income">Income</NavLink></li>
+    </ul>    
+  </header>
+);
+
+export default Header;
---
+++ components/PopulationContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {BootstrapTable,TableHeaderColumn}  from 'react-bootstrap-table';
+
+class PopulationContainer extends Component {    
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData:this.props.data
+    }
+  }
+  render() {
+    const data = this.state.tableData;
+    console.log(data,this.props)
+    const cellEditProp = {
+      mode: 'click',
+      blurToSave: true
+    };
+    const selectRowProp = {
+      mode: 'checkbox'
+    };
+    return (
+      <div className="main-content home">
+        <h2>{this.props.title}</h2>
+          <BootstrapTable data={ data } keyField='id' cellEdit={ cellEditProp } insertRow={ true } deleteRow={ true } selectRow={ selectRowProp }> 
+            {
+              Object.keys(data[0]).map(function(key,index) {
+                return (<TableHeaderColumn key={index} dataField={key} width='150'>{key}</TableHeaderColumn>)
+              })
+            }
+          </BootstrapTable>
+      </div>
+    );
+  }
+}
+
+export default PopulationContainer;
